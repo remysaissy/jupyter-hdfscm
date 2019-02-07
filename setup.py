e92d997b345f1af5d85d5e5d5c560e051c13102f
Rémy SAISSY <remy.saissy@gmail.com>
2019-02-07 10:57:23 +0100

    Add Python 3 as a minimum requirement in setup.py

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
         'Operating System :: Unix',
         'Operating System :: POSIX',
         'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Utilities',
@@ -61,7 +61,7 @@ setup(
     keywords=[
         'hdfs', 'jupyter'
     ],
-    python_requires='!=2.7.*, >=3.6',
+    python_requires='>=3.6',
     install_requires=[
         'tornado', 'traitlets', 'notebook', 'ipython_genutils', 'nbformat',
         'pyarrow'  # HDFS Support
