1b9abd924ada1f75b9bad186e27ee534afa922d5
Rémy Saissy <r.saissy@criteo.com>
2019-02-13 04:07:51 +0100

    Fix dev status, OS and Python version supported.

--- setup.py
+++ setup.py
@@ -42,12 +42,10 @@ setup(
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
